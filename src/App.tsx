7d7df2095294a35c9eed8c36d5fe2275ec5967a3
Nistha <nistha984475@gmail.com>
2024-10-25 13:05:42 +0545

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import Homepage from "./pages/homepage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Aboutpage from "./pages/aboutpage";
-import Blogpage from "./pages/blogpage";
-import Contactpage from "./pages/contactpage";
-import Layout from "./layout/layout";
-import Notfoundpage from "./pages/notfoundpage";
+import Approutes from "./routes/approutes";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/about" element={<Aboutpage />} />
-          <Route path="/blog" element={<Blogpage />} />
-          <Route path="/contact" element={<Contactpage />} />
-        </Route>
-        <Route path="*" element={<Notfoundpage />} />
-      </Routes>
+      <Approutes />
     </BrowserRouter>
   );
 };
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbarpage from "../pages/navbarpage";
 import { Outlet } from "react-router-dom";
 
