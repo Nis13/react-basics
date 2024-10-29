b08fb3ad444f64c26e2a148e26517b811316f067
Nistha <nistha984475@gmail.com>
2024-10-29 11:59:23 +0545

    add Yup validation

--- src/pages/userpage.tsx
+++ src/pages/userpage.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { PostUser } from "../hooks/userapi";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import Select from "react-select";
-import { useFormik } from "formik";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 
 const Userpage = () => {
   const options = [
@@ -12,83 +13,130 @@ const Userpage = () => {
   ];
   const [responseStatus, setResponseStatus] = useState<string | null>(null);
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      address: "",
-      contact: 0,
-      email: "",
-      gender: options[0].value,
-    },
-    onSubmit: async (values) => {
-      const result = await PostUser(values);
-      setResponseStatus(result);
-    },
+  const registerSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    address: Yup.string().required("Address is Required"),
+    email: Yup.string()
+      .email("should be in email format eg: abc@mail.com")
+      .required("Email is Required"),
+    gender: Yup.string(),
+    contact: Yup.number()
+      .positive()
+      .integer()
+      .test(
+        "len",
+        "Must be exactly 10 digits",
+        (val) => val?.toString().length === 10
+      )
+      .required("Required"),
   });
 
   return (
     <Container>
-      <Typography variant="h3">Login Account</Typography>
-      <form onSubmit={formik.handleSubmit}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "60%",
-            gap: "2rem",
-          }}
-        >
-          <TextField
-            label="name"
-            type="text"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            required
-          />
-          <TextField
-            label="email"
-            type="email"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            required
-          />
-          <TextField
-            label="address"
-            type="text"
-            name="address"
-            value={formik.values.address}
-            onChange={formik.handleChange}
-            required
-          />
-          <TextField
-            label="contact"
-            type="number"
-            name="contact"
-            value={formik.values.contact}
-            onChange={formik.handleChange}
-            required
-          />
+      <Typography variant="h3">Register Account</Typography>
+      <Formik
+        initialValues={{
+          name: "",
+          address: "",
+          contact: 0,
+          email: "",
+          gender: options[0].value,
+        }}
+        validationSchema={registerSchema}
+        onSubmit={async (values, { resetForm }) => {
+          const result = await PostUser(values);
+          setResponseStatus(result);
+          resetForm();
+        }}
+      >
+        {(props) => {
+          const {
+            values,
+            setFieldValue,
+            handleChange,
+            errors,
+            touched,
+            isSubmitting,
+          } = props;
+          return (
+            <Form>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  maxWidth: "60%",
+                  gap: "2rem",
+                }}
+              >
+                <TextField
+                  label="name"
+                  type="text"
+                  name="name"
+                  value={values.name}
+                  onChange={handleChange}
+                />
+                {errors.name && touched.name ? (
+                  <Box color={"red"}>{errors.name}</Box>
+                ) : null}
 
-          <Select
-            options={options}
-            defaultValue={options[0]}
-            name="gender"
-            isSearchable={false}
-            onChange={(option) => formik.setFieldValue("gender", option?.value)}
-          />
-          <Box>
-            <Button
-              variant="contained"
-              type="submit"
-              sx={{ backgroundColor: "" }}
-            >
-              Login
-            </Button>
-          </Box>
-        </Box>
-      </form>
+                <TextField
+                  label="email"
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                />
+                {errors.email && touched.email ? (
+                  <Box color={"red"}>{errors.email}</Box>
+                ) : null}
+
+                <TextField
+                  label="address"
+                  type="text"
+                  name="address"
+                  value={values.address}
+                  onChange={handleChange}
+                />
+                {errors.address && touched.address ? (
+                  <Box color={"red"}>{errors.address}</Box>
+                ) : null}
+
+                <TextField
+                  label="contact"
+                  type="number"
+                  name="contact"
+                  value={values.contact}
+                  onChange={handleChange}
+                />
+                {errors.contact && touched.contact ? (
+                  <Box color={"red"}>{errors.contact}</Box>
+                ) : null}
+
+                <Select
+                  options={options}
+                  defaultValue={options[0]}
+                  name="gender"
+                  isSearchable={false}
+                  onChange={(option) => setFieldValue("gender", option?.value)}
+                />
+                {errors.gender && touched.gender ? (
+                  <Box color={"red"}>{errors.gender}</Box>
+                ) : null}
+                <Box>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{ backgroundColor: "" }}
+                    disabled={isSubmitting}
+                  >
+                    Login
+                  </Button>
+                </Box>
+              </Box>
+            </Form>
+          );
+        }}
+      </Formik>
       {responseStatus && <p>{responseStatus}</p>}
     </Container>
   );
